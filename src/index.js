f10e27f0316251144b89d0a7d56b612e38e4fe5b
Fernando Mauro <lopezmauro1973f@gmail.com>
2022-10-25 22:59:31 -0500

    Los archivos iniciales estan listos y el servidor funciona bien

--- src/index.js
+++ src/index.js
@@ -1,2 +1,22 @@
-const Express = require('express');
-const app = Express();
+const express = require('express');
+const {createServer} = require('http');
+const {Server} = require('socket.io');
+
+const app = express();
+const httpServer = createServer(app);
+const io = new Server(httpServer); 
+
+const path = require('path');
+app.use(express.static(path.join(__dirname, 'views')))
+
+// servidor con http
+app.get('/', (req, res) => {
+   res.sendFile(__dirname + "/views/index.html")
+});
+
+// Servidor con web sockets
+io.on('connection', socket => {
+   console.log(socket.id);
+});
+
+httpServer.listen(3000);
