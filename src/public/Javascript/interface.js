070c8a4d06ad85dea1b98e63a2b8fa0c846e7aff
Fernando Mauro <lopezmauro1973f@gmail.com>
2022-12-09 15:49:30 -0600

    Interface archive create

---
+++ src/public/Javascript/interface.js
@@ -0,0 +1,7 @@
+const socket = io();
+
+let lastFirst, lastSecond;
+let isMyTurn = false;
+let queueGame = [];
+let lastContainPiece = "";
+const piecesCode = ["one-ball", "two-balls", "three-balls", "four-balls", "five-balls", "six-balls"];
