a606c839eb6dd9b2838638b2f1521b4a55aa7736
Fernando Mauro <lopezmauro1973f@gmail.com>
2023-04-10 19:23:54 -0600

    EL nuevo modal para decidir en donde poner la ficha (arriba o abajo) ya esta hecho manualmente y no tiene ningun error, asi que no deberia de haber problemas al querer cerrar el modal

--- src/public/Javascript/tradicional.js
+++ src/public/Javascript/tradicional.js
@@ -42,15 +42,18 @@ socket.on("connectedRoom", (data) => {
    console.log(`Te has unido a la sala`);
    remakeDom(data)
 });
+
 // catch errors
 socket.on("error", (data) => {
    console.error(data);
 });
 
 function remakeDom(data) {
+   // Seleccionar el modal y ocultarlo
    const container = document.querySelector("#new-game-modal");
    const modal = new Modal(container);
    modal.hide();
+
    document.querySelector("[modal-backdrop]").remove();
    // Re-make the DOM
    document.querySelector("#main-container").remove();
@@ -101,6 +104,7 @@ socket.on("sendPieces", data => {
    spanActive.classList.add("block", "text-center", "mx-auto", "bg-green-400", "text-green-800", "text-md", "font-medium", "px-2.5", "py-0.5", "rounded", "hidden");
    spanActive.textContent = "Es tu turno";
    spanActive.setAttribute("id", "turn-enable");
+
    // Si no es el turno
    const spanInactive = document.createElement("span");
    spanInactive.classList.add("block", "text-center", "mx-auto", "bg-red-400", "text-red-800", "text-md", "font-medium", "px-2.5", "py-0.5", "rounded");
@@ -111,7 +115,7 @@ socket.on("sendPieces", data => {
    currentTurn.classList.add("inline-block", "text-center", "mx-auto", "bg-yellow-200", "text-red-800", "text-md", "font-medium", "px-2.5", "py-2.5", "rounded", "my-4");
    // currentTurn.textContent = "Panchito";
    currentTurn.setAttribute("id", "current-turn");
-   
+
    // saltar turno
    const skipButton = document.createElement("button");
    skipButton.classList.add("block", "text-center", "bg-blue-800", "text-white", "text-md", "font-medium", "px-2.5", "py-2.5", "rounded", "my-4");
@@ -172,9 +176,7 @@ socket.on("sendPieces", data => {
       containPiece.addEventListener("click", () => {
          if (isMyTurn) {
             if (queueGame.length != 0) {
-               const hiddenModal = document.getElementById("desition-modal");
-               const modal = new Modal(hiddenModal);
-               modal.show();
+               toggleCustomModal();
                // console.log(piece.path[1].childNodes[0],piece.path[1].childNodes[1]);
                lastFirst = piece.first;
                lastSecond = piece.second;
@@ -194,10 +196,11 @@ socket.on("sendPieces", data => {
 socket.on("changeCurrentTurn", (data) => {
    changeCurrentTurn(data.name);
 });
+
 function changeCurrentTurn(currentTurn) {
    const spanTurn = document.querySelector("#current-turn");
    spanTurn.innerText = `Es turno de: ${currentTurn}`;
-   if(currentTurn == nameUser){
+   if (currentTurn == nameUser) {
       activeTurn();
    }
 }
@@ -429,28 +432,19 @@ function construirCola(data) {
 }
 const pushHead = document.getElementById("push-head");
 const pushTail = document.getElementById("push-tail");
+
 pushHead.addEventListener("click", () => {
    comprobatePiece(lastFirst, lastSecond, "head");
-   const hiddenModal = document.getElementById("desition-modal");
-   const modal = new Modal(hiddenModal);
-   modal.hide();
-   let backDrop = document.querySelector("[modal-backdrop]");
-   backDrop.remove();
-   // let secondBackDrop = document.querySelector("[modal-backdrop]");
-   // secondBackDrop.remove();
+   toggleCustomModal();
    checkWin();
 });
+
 pushTail.addEventListener("click", () => {
    comprobatePiece(lastFirst, lastSecond, "tail");
-   const hiddenModal = document.getElementById("desition-modal");
-   const modal = new Modal(hiddenModal);
-   modal.hide();
-   let backDrop = document.querySelector("[modal-backdrop]");
-   backDrop.remove();
-   // let secondBackDrop = document.querySelector("[modal-backdrop]");
-   // secondBackDrop.remove();
+   toggleCustomModal();
    checkWin();
 });
+
 socket.on("myTurn", () => {
    activeTurn();
 });
@@ -471,59 +465,59 @@ function isValid(piece) {
    }
 }
 socket.on("eatedPiece", (piece) => {
-      const piecesContainer = document.querySelector("#pieces-container");
-      const containPiece = document.createElement("div");
-      containPiece.classList.add("piece");
-      const topHalf = document.createElement("div");
-      topHalf.classList.add("top-half-column", `${piecesCode[piece.first - 1]}`);
-      for (let i = 0; i < piece.first; ++i) {
-         const bolita = document.createElement("div");
-         bolita.classList.add("bolita");
-         topHalf.appendChild(bolita);
-      }
+   const piecesContainer = document.querySelector("#pieces-container");
+   const containPiece = document.createElement("div");
+   containPiece.classList.add("piece");
+   const topHalf = document.createElement("div");
+   topHalf.classList.add("top-half-column", `${piecesCode[piece.first - 1]}`);
+   for (let i = 0; i < piece.first; ++i) {
+      const bolita = document.createElement("div");
+      bolita.classList.add("bolita");
+      topHalf.appendChild(bolita);
+   }
 
-      const bottomHalf = document.createElement("div");
-      bottomHalf.classList.add("bottom-half-column", `${piecesCode[piece.second - 1]}`);
-      for (let i = 0; i < piece.second; ++i) {
-         const bolita = document.createElement("div");
-         bolita.classList.add("bolita");
-         bottomHalf.appendChild(bolita);
-      }
+   const bottomHalf = document.createElement("div");
+   bottomHalf.classList.add("bottom-half-column", `${piecesCode[piece.second - 1]}`);
+   for (let i = 0; i < piece.second; ++i) {
+      const bolita = document.createElement("div");
+      bolita.classList.add("bolita");
+      bottomHalf.appendChild(bolita);
+   }
 
-      containPiece.appendChild(topHalf);
-      containPiece.appendChild(bottomHalf);
-      piecesContainer.appendChild(containPiece);
-      containPiece.addEventListener("click", () => {
-         if (isMyTurn) {
-            if (queueGame.length != 0) {
-               const hiddenModal = document.getElementById("desition-modal");
-               const modal = new Modal(hiddenModal);
-               modal.show();
-               // console.log(piece.path[1].childNodes[0],piece.path[1].childNodes[1]);
-               lastFirst = piece.first;
-               lastSecond = piece.second;
-               lastContainPiece = containPiece;
-            } else {
-               lastFirst = piece.first;
-               lastSecond = piece.second;
-               lastContainPiece = containPiece;
-               comprobatePiece(piece.first, piece.second, "middle");
-            }
+   containPiece.appendChild(topHalf);
+   containPiece.appendChild(bottomHalf);
+   piecesContainer.appendChild(containPiece);
+   containPiece.addEventListener("click", () => {
+      if (isMyTurn) {
+         if (queueGame.length != 0) {
+            const hiddenModal = document.getElementById("desition-modal");
+            const modal = new Modal(hiddenModal);
+            modal.show();
+            // console.log(piece.path[1].childNodes[0],piece.path[1].childNodes[1]);
+            lastFirst = piece.first;
+            lastSecond = piece.second;
+            lastContainPiece = containPiece;
+         } else {
+            lastFirst = piece.first;
+            lastSecond = piece.second;
+            lastContainPiece = containPiece;
+            comprobatePiece(piece.first, piece.second, "middle");
          }
-      });
+      }
+   });
 });
-function checkWin(){
+function checkWin() {
    const piecesContainer = document.querySelector("#pieces-container");
    console.log("comprobando");
-   if(piecesContainer.childElementCount == 0){
+   if (piecesContainer.childElementCount == 0) {
       const modal = document.querySelector("#wined-game");
       const smmodal = new Modal(modal);
       smmodal.show();
-      socket.emit("winner", {name: nameUser});
+      socket.emit("winner", { name: nameUser });
    }
 }
 socket.on("winner", (data) => {
-   if(data.name != nameUser){
+   if (data.name != nameUser) {
       const textwiner = document.querySelector("#name-winer");
       textwiner.textContent = `${data.name} ha ganado!`;
       const modal = document.querySelector("#lose-game");
@@ -531,3 +525,15 @@ socket.on("winner", (data) => {
       smmodal.show();
    }
 })
+
+const toggleCustomModal = () => {
+   const modal = document.querySelector("#desition-modal");
+   const overlay = document.querySelector("#overlay")
+
+   modal.classList.toggle("hidden");
+   overlay.classList.toggle("hidden");
+}
+
+// Cerra el modal si se presiona afuera
+const overlay = document.querySelector("#overlay")
+overlay.addEventListener("click", toggleCustomModal)
