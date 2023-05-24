ac09a1349b7d18d729c098caa42c3dce3de2ac83
Fernando Mauro <lopezmauro1973f@gmail.com>
2023-05-23 22:16:27 -0600

    CheckisClosed no esta terminado, solo me interesa una sola mitad del head y tail, no me interesan ambas

--- src/createNewGame.js
+++ src/createNewGame.js
@@ -1,6 +1,6 @@
 const Player = require("./player.js");
 
-function returnRandomPiece(min,max) {
+function returnRandomPiece(min, max) {
    return Math.floor(Math.random() * (max - min) + min);
 }
 
@@ -18,12 +18,58 @@ class Game {
       this.notUsed = [];
       this.modeGame = modeGame;
    }
-   
-   checkIsClosed(){
-      console.log('-----------------------------------------------------');
+
+   checkIsClosed() {
+      const firstHead = typeof (this.queueGame[0].first) === "string" ? parseInt(this.queueGame[0].first.at(-1)) : this.queueGame[0].first;
+      const secondHead = typeof (this.queueGame[0].second) === "string" ? parseInt(this.queueGame[0].second.at(-1)) : this.queueGame[0].second;
+      const head = {
+         first: firstHead,
+         second: secondHead
+      };
+
+      const firstTail = typeof (this.queueGame.at(-1).first) === "string" ? parseInt(this.queueGame.at(-1).first.at(-1)) : this.queueGame.at(-1).first;
+      const secondTail = typeof (this.queueGame.at(-1).second) === "string" ? parseInt(this.queueGame.at(-1).second.at(-1)) : this.queueGame.at(-1).second;
+      const tail = {
+         first: firstTail,
+         second: secondTail
+      };
+      console.log(tail,head);
+      let bandera = true;
+
+      // Iterar sobre las piezas no utilizadas
+      for (const notUse of this.notUsed) {
+         // comprobar con head
+         if (notUse.first == head.first && notUse.second == head.second || notUse.second == head.first && notUse.first == head.second) {
+            bandera = false;
+         }
+
+         // comprobar con tail
+         if (notUse.first == tail.first && notUse.second == tail.second || notUse.second == tail.first && notUse.first == tail.second) {
+            bandera = false;
+         }
+      }
+
+      // Iterar sobre las manos de ambos jugadores
       this.players.forEach(player => {
-         console.log(player.hand);
-      })
+         player.hand.forEach(notUse => {
+            // comprobar con head
+            if (notUse.first == head.first && notUse.second == head.second || notUse.second == head.first && notUse.first == head.second) {
+               bandera = false;
+            }
+
+            // comprobar con tail
+            if (notUse.first == tail.first && notUse.second == tail.second || notUse.second == tail.first && notUse.first == tail.second) {
+               bandera = false;
+            }
+         })
+      });
+
+      if(bandera){
+         console.log('El juego se ha cerrado');
+      }else{
+         console.log('EL juego sigue');
+      }
+      return bandera;
    }
 
    // Generar las piezas
@@ -46,16 +92,16 @@ class Game {
       // Generar las 7 fichas para cada jugador
       this.players.forEach((player, index) => {
          while (player.hand.length < 7) {
-            let positionRandom = returnRandomPiece(0,28);
+            let positionRandom = returnRandomPiece(0, 28);
 
             while (this.piezas[positionRandom].used && positionRandom < 28) {
                positionRandom = positionRandom === 27 ? 0 : positionRandom + 1;
             }
             // Si aleatoriamente se asigna la mula del 6 el primer turno es del jugador con la mula del 6
-            if (this.piezas[positionRandom].first === 6 && this.piezas[positionRandom].second === 6){
+            if (this.piezas[positionRandom].first === 6 && this.piezas[positionRandom].second === 6) {
                this.turn = index;
                this.mula = true;
-            } 
+            }
             player.hand.push(this.piezas[positionRandom]);
             this.piezas[positionRandom].used = true;
             this.numberUse++;
@@ -64,18 +110,18 @@ class Game {
 
       // Agregar al set las piezas que no se estan ocupando
       this.piezas.forEach(pieza => {
-         if(!pieza.used){
+         if (!pieza.used) {
             this.notUsed.push(pieza);
          }
       });
-      
+
       // Si no se repartio la mula del 6
       let bandera = true;
-      if(!this.mula){
-         for(let i = 5; i >= 0; --i){
+      if (!this.mula) {
+         for (let i = 5; i >= 0; --i) {
             this.players.forEach((player, index) => {
                player.hand.forEach(pieza => {
-                  if(pieza.first == i && pieza.second == i && bandera){
+                  if (pieza.first == i && pieza.second == i && bandera) {
                      this.turn = index;
                      bandera = false;
                      return;
@@ -85,10 +131,10 @@ class Game {
          }
       }
    }
-   deletePieceFromHand({first, second}){
+   deletePieceFromHand({ first, second }) {
       this.players.forEach(player => {
          player.hand.forEach((piece, index) => {
-            if(piece.first == first && piece.second == second || piece.first == second && piece.second == first){
+            if (piece.first == first && piece.second == second || piece.first == second && piece.second == first) {
                console.log('llegue');
                player.hand.splice(index, 1);
                return;
@@ -106,6 +152,7 @@ class Game {
                   this.queueGame.at(-1).first = "*";
                   this.queueGame.push(piece);
                   this.deletePieceFromHand(piece);
+                  this.checkIsClosed();
                   this.nextTurn();
                   return {
                      side: "tail",
@@ -116,6 +163,7 @@ class Game {
                   this.queueGame.at(-1).second = "*";
                   this.queueGame.push(piece);
                   this.deletePieceFromHand(piece);
+                  this.checkIsClosed();
                   this.nextTurn();
                   return {
                      side: "tail",
@@ -126,6 +174,7 @@ class Game {
                   this.queueGame.at(-1).first = "*";
                   this.queueGame.push(piece);
                   this.deletePieceFromHand(piece);
+                  this.checkIsClosed();
                   this.nextTurn();
                   return {
                      side: "tail",
@@ -136,6 +185,7 @@ class Game {
                   this.queueGame.at(-1).second = "*";
                   this.queueGame.push(piece);
                   this.deletePieceFromHand(piece);
+                  this.checkIsClosed();
                   this.nextTurn();
                   return {
                      side: "tail",
@@ -151,6 +201,7 @@ class Game {
                   this.queueGame[0].first = "*";
                   this.queueGame.unshift(piece);
                   this.deletePieceFromHand(piece);
+                  this.checkIsClosed();
                   this.nextTurn();
                   return {
                      side: "head",
@@ -161,6 +212,7 @@ class Game {
                   this.queueGame[0].second = "*";
                   this.queueGame.unshift(piece);
                   this.deletePieceFromHand(piece);
+                  this.checkIsClosed();
                   this.nextTurn();
                   return {
                      side: "head",
@@ -171,6 +223,7 @@ class Game {
                   this.queueGame[0].first = "*";
                   this.queueGame.unshift(piece);
                   this.deletePieceFromHand(piece);
+                  this.checkIsClosed();
                   this.nextTurn();
                   return {
                      side: "head",
@@ -180,7 +233,8 @@ class Game {
                   piece.second = "*" + piece.second;
                   this.queueGame[0].second = "*";
                   this.queueGame.unshift(piece);
-                  this.deletePieceFromHand(piece); 
+                  this.deletePieceFromHand(piece);
+                  this.checkIsClosed();
                   this.nextTurn();
                   return {
                      side: "head",
@@ -190,34 +244,35 @@ class Game {
                   this.players[this.turn].socketPlayer.emit("badPiece");
                }
             }
-         }else {
+         } else {
             this.queueGame.push({
                first: piece.first,
                second: piece.second
             });
             this.deletePieceFromHand(piece)
+            this.checkIsClosed();
             this.nextTurn();
             return {
                side: "middle",
                half: undefined
             };
          }
-      } 
+      }
 
    }
    nextTurn() {
       this.turn++;
-      if(this.turn >= this.players.length) {
+      if (this.turn >= this.players.length) {
          this.turn = 0;
       }
       // this.players[this.turn].socketPlayer.emit("myTurn", this.players[this.turn].name);
       this.players.forEach((player) => {
-         player.socketPlayer.emit("changeCurrentTurn", {name: this.players[this.turn].name});
+         player.socketPlayer.emit("changeCurrentTurn", { name: this.players[this.turn].name });
       })
    }
-   eatPieces(){
-      if(this.numberUse <= 28){
-         const randomNumber = returnRandomPiece(0, 28-this.numberUse);
+   eatPieces() {
+      if (this.numberUse <= 28) {
+         const randomNumber = returnRandomPiece(0, 28 - this.numberUse);
          this.numberUse++;
          const last = this.notUsed[randomNumber];
          this.notUsed.splice(randomNumber, 1);
