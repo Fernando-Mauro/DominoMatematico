dea00a237115c082d2b36e686d37a8931d7c1a95
Fernando Mauro <fermaurolf@gmail.com>
2023-08-13 11:49:37 -0600

    Ya funciona con cualquier variacion del domino la aplicacion

--- public/js/DOM/fillImg.js
+++ public/js/DOM/fillImg.js
@@ -1,3 +1,16 @@
-export const fillImg = (half, points, gameMode) => {
-    half.style.backgroundImage = `url(/assets/${gameMode}/${points}.png)`;
+export const fillImg = async (half, points, gameMode) => {
+    let response;
+    try {
+        response = await fetch(`/assets/${gameMode}/${points}/${points}.svg`);
+    } catch (error) {
+        console.log('There was an error', error);
+    }
+    let url = "";
+    if (response?.ok) {
+        url = `/assets/${gameMode}/${points}/${points}.svg`
+    } else {
+        url = `/assets/${gameMode}/${points}.svg`
+    }
+
+    half.style.backgroundImage = `url(${url})`;
 }
--- src/Routes/index.js
+++ src/Routes/index.js
@@ -7,4 +7,9 @@ const flowbite = require("./flowbite");
 // Rutas especificas :D
 router.use("/flowbite.js", flowbite);
 
+// router.use("/assets/**/**/*.svg", (req,res) => {
+//     console.log(req.originalUrl);
+//     res.sendFile(req.originalUrl)
+// })
+
 module.exports = router;
