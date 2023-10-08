97017413c234d8fa59a58ac55766daacd9be82fe
Anai Hernández Peralta <anaihernandezperalta@gmail.com>
2023-10-08 12:11:32 -0600

    Problema de los elementos fixed y el botón learn more solucionado

--- public/js/landingpage.js
+++ public/js/landingpage.js
@@ -2,10 +2,13 @@ const swiper = new Swiper('.swiper', {
     // Optional parameters
     direction: 'horizontal',
     loop: true,
+    effect: "fade",
 
     // If we need pagination
     pagination: {
         el: '.swiper-pagination',
+        clickable: true,
+        dynamicBullets: true,
     },
 
     // Navigation arrows
