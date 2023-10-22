e5930b0847aa312c829809f98631f48ff92661d2
Fernando Mauro <fermaurolf@gmail.com>
2023-10-21 20:16:34 -0600

    Routes and some changes in the landing page for responsive design

--- src/Routes/index.js
+++ src/Routes/index.js
@@ -4,14 +4,17 @@ const router = Router();
 // importar
 const flowbite = require("./flowbite");
 const swiper = require("./swiper.js")
-const swipercss = require("./swipercss.js")
-
+const swipercss = require("./swipercss.js");
+const landingPage = require("./landingPage.js");
 
 // Rutas especificas :D
+router.use("/", landingPage)
+
 router.use("/flowbite.js", flowbite);
 
 router.use("/swiper.js", swiper)
 
 router.use("/swiper.css", swipercss)
+
 
 module.exports = router;
