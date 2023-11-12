0336f00203bc545b03df50a91efd94dcd1a8c3bb
Fernando Mauro <fermaurolf@gmail.com>
2023-11-12 11:55:16 -0600

    New menu

--- public/index.js
+++ public/index.js
@@ -1,28 +1,64 @@
-const anchors = document.querySelectorAll(".anchor");
 
+const levelSelector = document.querySelectorAll(".level-selector");
 const removeAccents = (str) => {
-  return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
-} 
+    return str.normalize("NFD").replace(/[\u0300-\u036f]/g, "");
+}
 
-anchors.forEach((el) => {
-    const innerText  = removeAccents(el.innerText.toLowerCase());
+levelSelector.forEach(el => {
     el.addEventListener("click", () => {
-        setGameMode(innerText);
+        const level = el.childNodes[1].innerText.split(" ")[1];
+        remakeDom(level);
     })
-})
+});
+
 
 const setGameMode = (gameMode) => {
     sessionStorage.setItem("gameMode", gameMode);
     let extension = undefined;
-    if(gameMode === "princesas" || gameMode === "heroes" || gameMode === "ecuaciones"){
+    if (gameMode === "princesas" || gameMode === "heroes" || gameMode === "ecuaciones") {
         extension = "png";
-    }else{
+    } else {
         extension = "svg";
     }
     sessionStorage.setItem("extension", extension);
     let nested = "false";
-    if(gameMode === "ecuaciones" || gameMode === "algebra"){
+    if (gameMode === "ecuaciones" || gameMode === "algebra") {
         nested = "true";
     }
     sessionStorage.setItem("nested", nested);
 }
+
+const remakeDom = async (level) => {
+    const levelLower = level.toLowerCase();
+    try {
+        const response = await fetch("./levels.json");
+        const data = await response.json();
+
+        if (!data[0][levelLower]) return 0;
+
+        const containerCards = document.querySelector(".container-cards");
+        document.querySelectorAll(".card").forEach(el => el.remove());
+
+        let stringComplete = ""
+        data[0][levelLower].forEach(el => {
+            // containerCards.appendChild(el);
+            stringComplete += el;
+        });
+        containerCards.innerHTML = stringComplete;
+
+        const anchors = document.querySelectorAll(".anchor");
+
+        anchors.forEach((el) => {
+            const innerText = removeAccents(el.innerText.toLowerCase());
+            el.addEventListener("click", () => {
+                setGameMode(innerText);
+            })
+        })
+    } catch (error) {
+        console.error('Error al cargar el archivo JSON:', error);
+    }
+}
+
+document.addEventListener("DOMContentLoaded", () => {
+    remakeDom("preescolar");
+})
